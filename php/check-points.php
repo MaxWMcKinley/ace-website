5d81e7336826352df21e5b421c5937613ba70e52
maxwmckinley <maxwmckinley@gmail.com>
2016-10-07 22:18:13 -0500

    Improvements to points checker

--- php/check-points.php
+++ php/check-points.php
@@ -30,12 +30,39 @@ else {	// No result from database
 		die("Nothing in database result when trying to get UIN");
 }
 
+// Get all the points of the ACE member
 $query = "SELECT * FROM `$usertable` where `uin`='$uin'";
 $result = mysql_query($query);
 
 if ($result) {
 	$row = mysql_fetch_array($result);
 }
+
+// Calculate total points of ACE members
+$total = $row['recruitment'] + $row['service'] + $row['fundraising'] + $row['academic'] + $row['athletics'] + $row['social'] + $row['tailgate'] + $row['corporate_relations'] + $row['pr'] + $row['songfest'] + $row['special_events'] + $row['attendance'] + $row['attendance'] + $row['family'] + $row['sober_rides'] + $row['flex'];
+
+// Calculate the points still needed for each category
+$recruitment = 0;
+$service = 25 - $row['service'];
+if ($service < 0) $service = 0;
+$fundraising = 20 - $row['fundraising'];
+if ($fundraising < 0) $fundraising = 0;
+$academic = 0;
+$athletics = 0;
+$social = 5 - $row['social'];
+if ($social < 0) $social = 0;
+$tailgate = 0;
+$corporate_relations = 0;
+$pr = 5 - $row['pr'];
+if ($pr < 0) $pr = 0;
+$songfest = 0;
+$special_events = 0;
+$attendance = 8 - $row['attendance'];
+if ($attendance < 0) $attendance = 0;
+$family = (7 - $row['family']);
+if ($family < 0) $family = 0;
+$sober_rides = 0;
+$flex = 0;
 
 // Output html table with point information
 echo "<h2>Points You Have</h2>";
@@ -59,7 +86,7 @@ echo "<table>";
 		echo "<th>Flex</th>";
 	echo "</tr>";
 	echo "<tr>";
-		echo "<td>" . $row['required'] . "</td>";
+		echo "<td>" . $total . "</td>";
 		echo "<td>" . $row['recruitment'] . "</td>";
 		echo "<td>" . $row['service'] . "</td>";
 		echo "<td>" . $row['fundraising'] . "</td>";
@@ -75,6 +102,46 @@ echo "<table>";
 		echo "<td>" . $row['family'] . "</td>";
 		echo "<td>" . $row['sober_rides'] . "</td>";
 		echo "<td>" . $row['flex'] . "</td>";
+	echo "</tr>";
+echo "</table>";
+echo "<br><br>";
+echo "<h2>Points You Still Need</h2>";
+echo "<table>";
+	echo "<tr>";
+		echo "<th>Total</th>";
+		echo "<th>Recruitment</th>";
+		echo "<th>Service</th>";
+		echo "<th>Fundraising</th>";
+		echo "<th>Academic</th>";
+		echo "<th>Athletics</th>";
+		echo "<th>Social</th>";
+		echo "<th>Tailgate</th>";
+		echo "<th>Corporate Relations</th>";
+		echo "<th>PR</th>";
+		echo "<th>Songfest</th>";
+		echo "<th>Special Events</th>";
+		echo "<th>Attendance</th>";
+		echo "<th>Family</th>";
+		echo "<th>Sober Rides</th>";
+		echo "<th>Flex</th>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td>" . ($row['required'] - $total) . "</td>";
+		echo "<td>" . $recruitment . "</td>";
+		echo "<td>" . $service . "</td>";
+		echo "<td>" . $fundraising . "</td>";
+		echo "<td>" . $academic . "</td>";
+		echo "<td>" . $athletics . "</td>";
+		echo "<td>" . $social . "</td>";
+		echo "<td>" . $tailgate . "</td>";
+		echo "<td>" . $corporate_relations . "</td>";
+		echo "<td>" . $pr . "</td>";
+		echo "<td>" . $songfest . "</td>";
+		echo "<td>" . $special_events . "</td>";
+		echo "<td>" . $attendance . "</td>";
+		echo "<td>" . $family . "</td>";
+		echo "<td>" . $sober_rides . "</td>";
+		echo "<td>" . $flex . "</td>";
 	echo "</tr>";
 echo "</table>";
 ?>

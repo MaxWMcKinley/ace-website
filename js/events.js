ed9d4464c98cca94333d1b012d5dda1737207824
Max McKinley <MaxWMcKinley@gmail.com>
2017-01-14 17:14:43 -0600

    Added autocomplete to create-event

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@ $.getJSON("../php/get-events.php", function(result){
 	var events = "";
 	$.each( result, function( key, value ) {
   		events += `
-			<button type='button' class='list-group-item' data-toggle='modal' data-target='#eventModal' data-name='${key}' data-officername='${value.officer_name}' data-date='${value.date}' data-freeze='${value.freeze}' data-type='${value.type}' data-points='${value.points}'>
+			<button type='button' class='list-group-item' data-toggle='modal' data-target='#eventModal' data-name='${key}' data-id='${value.id}' data-officername='${value.officer_name}' data-date='${value.date}' data-freeze='${value.freeze}' data-type='${value.type}' data-points='${value.points}'>
 				<span class='badge'>${value.type}</span>
 				<h2 class='list-group-item-heading'>${key}</h2>
 					<p class='list-group-item-text'>${value.date}  |  ${value.points} points</p>
@@ -13,16 +13,21 @@ $.getJSON("../php/get-events.php", function(result){
 });
 
 $('#eventModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var modal = $(this)
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var modal = $(this);
 
-  var name = button.data('name') // Extract info from data-* attributes
+  var name = button.data('name'); // Extract info from data-* attributes
+  var id = button.data('id');
   var officer_name = button.data('officername');
-  var date = button.data('date') // Extract info from data-* attributes
-  var freeze = button.data('freeze') // Extract info from data-* attributes
-  var type = button.data('type') // Extract info from data-* attributes
-  var points = button.data('points') // Extract info from data-* attributes
+  var date = button.data('date'); // Extract info from data-* attributes
+  var freeze = button.data('freeze'); // Extract info from data-* attributes
+  var type = button.data('type'); // Extract info from data-* attributes
+  var points = button.data('points'); // Extract info from data-* attributes
 
+  $.getJSON("../php/get-shifts.php", function(result){
+
+  });
+  
 	var shifts = "";
 	// for (var i = start; i < end; i+=shiftLength) {
 	// 	shifts += `

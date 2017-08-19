b9e550b8c8f397b0820c22b456030767e2325a0c
Max McKinley <MaxWMcKinley@gmail.com>
2017-08-19 02:54:44 -0500

    Adding sample file to connect to database

---
+++ php/connect-sample.php
@@ -0,0 +1,12 @@
+<?php
+
+// Setting connection variables
+$hostname="";
+$username="";
+$password="";
+$dbname="";
+
+// Connecting to database
+$conn = new mysqli($hostname, $username, $password, $dbname);
+if ($conn->connect_errno)
+	echo "Failed to connect to database with error number " . $conn->connect_errno . " (" . $conn->connect_error . ")";
